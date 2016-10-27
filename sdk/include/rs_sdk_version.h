7dca9315b802a282d7bc251246174891c3e38169
Katz <perc_tfs_support@intel.com>
2016-10-27 17:20:25 +0300

    update version file

--- sdk/include/rs_sdk_version.h
+++ sdk/include/rs_sdk_version.h
@@ -3,9 +3,9 @@
 
 #pragma once
 
-#define SDK_VER_MAJOR   0
-#define SDK_VER_MINOR   3
-#define SDK_VER_PATCH   0
-#define SDK_VERSION_STRING static volatile char version_id[] = "VERSION: 0.3.0";
+#define SDK_VER_MAJOR 0
+#define SDK_VER_MINOR 4
+#define SDK_VER_PATCH 0
+#define SDK_VERSION_STRING static volatile char version_id[] = "VERSION: 0.4.0";
 
 SDK_VERSION_STRING
