97e88d97437b6eb7e2bbf2b4dbc77c58ee533751
Smirnov, Michail <michail.smirnov@intel.com>
2016-11-22 13:31:07 +0200

    Updated rs_sdk_version.h

--- sdk/include/rs_sdk_version.h
+++ sdk/include/rs_sdk_version.h
@@ -7,7 +7,7 @@
 #define SDK_VER_MINOR   3
 #define SDK_VER_PATCH   0
 #define SDK_VERSION_STRING   static volatile char version_id[] = "VERSION: 0.3.0";
-#define SDK_BUILD_STRING     static volatile char build[]= "BUILD DATA: " __DATE__ " " __TIME__;
+#define SDK_BUILD_STRING     static volatile char build[]= "BUILD DATE: " __DATE__ " " __TIME__;
 #define SDK_COPYRIGHT_STRING static volatile char copyright[]="COPYRIGHT: Intel Confidential Copyright 2016"; 
 
 
