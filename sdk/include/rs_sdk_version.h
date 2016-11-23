3f8047860d69d9a61b607b4aadc89ba3d604d2a3
Bolshem <perc_tfs_support@intel.com>
2016-11-23 18:29:28 +0200

    update version file

--- sdk/include/rs_sdk_version.h
+++ sdk/include/rs_sdk_version.h
@@ -3,14 +3,9 @@
 
 #pragma once
 
-#define SDK_VER_MAJOR   0
-#define SDK_VER_MINOR   3
-#define SDK_VER_PATCH   0
-#define SDK_VERSION_STRING   static volatile char version_id[] = "VERSION: 0.3.0";
-#define SDK_BUILD_STRING     static volatile char build[]= "BUILD DATE: " __DATE__ " " __TIME__;
-#define SDK_COPYRIGHT_STRING static volatile char copyright[]="COPYRIGHT: Intel Confidential Copyright 2016"; 
-
+#define SDK_VER_MAJOR 0
+#define SDK_VER_MINOR 6
+#define SDK_VER_PATCH 0
+#define SDK_VERSION_STRING static volatile char version_id[] = "VERSION: 0.6.0";
 
 SDK_VERSION_STRING
-SDK_BUILD_STRING
-SDK_COPYRIGHT_STRING
