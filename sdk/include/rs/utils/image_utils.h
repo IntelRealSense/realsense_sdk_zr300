95beac079c55f018b598215535da695e49f88e5a
Eldar Zilberman <eldar.zilberman@intel.com>
2016-12-08 10:55:56 +0200

    get pixel size documentation

--- sdk/include/rs/utils/image_utils.h
+++ sdk/include/rs/utils/image_utils.h
@@ -1,14 +1,24 @@
 // License: Apache 2.0. See LICENSE file in root directory.
 // Copyright(c) 2016 Intel Corporation. All Rights Reserved.
 
-#pragma once
 
+/**
+* \file image_utils.h
+* @brief Describes utilities for images.
+*/
+
+#pragma once
 #include "rs/core/types.h"
 
 namespace rs
 {
     namespace utils
     {
+        /**
+        * @brief Providing pixel byte size for a given pixel format.
+        * @param[in] format     Pixel format.
+        * @return int8_t        Byte size of the given pixel format.
+        */
         static int8_t get_pixel_size(rs::core::pixel_format format)
         {
             switch(format)
