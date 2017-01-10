8e0e721dc87056b0f7977d637842c44e8294bcdf
Ziv, Shahaf <ziv.shahaf@intel.com>
2017-01-10 18:16:27 +0200

    Revert "PR 915: Merge ziv_dev_external_cam_sample to development"
    This reverts commit b5a59620a302120e04d555a082c38a93a74b97c8, reversing
    changes made to 8d6ef3f5ffdab5c0f552a8d07a940cb84e8e0575.
    
    NOTE: The following files had to be manually changed even though they were not part of the reverted commit:
    	modified:   sdk/include/rs_core.h
    	modified:   sdk/src/tools/projection_tool/projection_tool.cpp

--- sdk/src/utilities/samples_time_sync/samples_time_sync_impl.cpp
+++ sdk/src/utilities/samples_time_sync/samples_time_sync_impl.cpp
@@ -5,7 +5,6 @@
 #include "samples_time_sync_zr300.h"
 #include "samples_time_sync_ds5.h"
 #include "rs_sdk_version.h"
-#include "samples_time_sync_external_camera.h"
 
 namespace rs {
     namespace utils {
@@ -19,18 +18,8 @@ namespace rs {
                 std::string str(device_name);
                 if ( str.find("ZR300") != std::string::npos )
                         return new samples_time_sync_zr300(streams_fps, motions_fps, max_input_latency, not_matched_frames_buffer_size);
-
-                if ( str.compare(external_device_name) == 0 )
-                {
-                    //External camera synchronization requires that a single buffer is used for the images arrays
-                    const int SINGLE_BUFFER = 1;
-                    return new samples_time_sync_external_camera(streams_fps,
-                                                                 motions_fps, SINGLE_BUFFER,
-                                                                 not_matched_frames_buffer_size);
-                }
                 /*if ( str.find("RS400") != std::string::npos )
                         return new samples_time_sync_ds5(streams_fps, motions_fps, max_input_latency, not_matched_frames_buffer_size);*/
-
 
             }
 
